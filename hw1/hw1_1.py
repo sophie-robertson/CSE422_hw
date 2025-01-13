c8e9506187943f7ec798555b0c63cecab2dc5e83
sophie <slr1234@uw.edu>
2025-01-13 12:34:13 -0800

    sampling without replacement

--- hw1/hw1_1.py
+++ hw1/hw1_1.py
@@ -4,27 +4,38 @@ import matplotlib.pyplot as plt
 # Select one of the n bins uniformly at random
 def uniform(m, n):
     bins = np.zeros(n)
-    assignments = np.random.randint(low=0, high=n, size=(m,), dtype=int)
-    for b in range(n):
-        bins[b] = np.count_nonzero(assignments == b)
+    for ball in range(m):
+        bin1 = np.random.randint(0, n)
+        bins[bin1] += 1
     return np.max(bins)
 
-# Select two bins uniformly at random, and place the ball into whichever bin has fewer balls so far
+# Select two bins uniformly at random (implemented without replacement), and place the ball into whichever bin has fewer balls so far
 # (breaking ties arbitrarily).
 def two_choices(m, n):
     bins = np.zeros(n)
     for ball in range(m):
-        two_bins = np.random.randint(low=0, high=n, size=(2,), dtype=int)
-        bin_ind = two_bins[0] if bins[two_bins[0]] <= bins[two_bins[1]] else two_bins[1]
+        bin1 = np.random.randint(0, n)
+        bin2 = bin1
+        while bin2 == bin1:
+            bin2 = np.random.randint(0,n)
+        bin_ind = bin1 if bins[bin1] <= bins[bin2] else bin2
         bins[bin_ind] += 1
     return np.max(bins)
 
-# Select three bins uniformly at random, and place the ball into whichever bin has fewer balls so far
+# Select three bins uniformly at random (implemented without replacement), and place the ball into whichever bin has fewer balls so far
 # (breaking ties arbitrarily).
 def three_choices(m, n):
     bins = np.zeros(n)
+    sampler = np.arange(n)
     for ball in range(m):
-        three_bins = np.random.randint(low=0, high=n, size=(3,), dtype=int)
+        bin1 = np.random.randint(0, n)
+        bin2 = bin1
+        bin3 = bin1
+        while bin2 == bin1:
+            bin2 = np.random.randint(0,n)
+        while bin3 == bin1 or bin3 == bin2:
+            bin3 = np.random.randint(0,n)
+        three_bins = [bin1, bin2, bin3]
         counts = np.array([bins[three_bins[0]], bins[three_bins[1]], bins[three_bins[2]]])
         bin_ind = three_bins[np.argmin(counts)]
         bins[bin_ind] += 1
@@ -49,12 +60,20 @@ twochoices_maxload = np.zeros(n_trials)
 threechoices_maxload = np.zeros(n_trials)
 stratfour_maxload = np.zeros(n_trials)
 for j in range(n_trials):
-    if j % 10 == 0:
-        print("On iteration: " + str(j))
+    print("On iteration: " + str(j))
     uniform_maxload[j] = uniform(m, n)
+    print("\tUniform done")
     twochoices_maxload[j] = two_choices(m, n)
+    print("\tTwo choices done")
     threechoices_maxload[j] = three_choices(m, n)
+    print("\tThree choices done")
     stratfour_maxload[j] = strategy_four(m, n)
+    print("\tAdjacent done")
+
+print(uniform_maxload)
+print(twochoices_maxload)
+print(threechoices_maxload)
+print(stratfour_maxload)
 
 fig, axs = plt.subplots(2, 2)
 axs[0,0].hist(uniform_maxload)
